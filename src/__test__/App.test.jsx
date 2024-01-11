351a4b1ecc21f44d495379b654269afca746d97b
Avenez <145536778+Avenez@users.noreply.github.com>
2024-01-11 16:01:21 +0100

    secondo border check work

--- src/__test__/App.test.jsx
+++ src/__test__/App.test.jsx
@@ -2,18 +2,59 @@ import { fireEvent, render, screen } from "@testing-library/react";
 import App from "../App";
 import fantasy from "../data/fantasy.json";
 
-describe("check the cards of books", () => {
-  it("check if the number of cards rendered is correct", () => {
-    render(<App />);
+const getRandomCard = () => {
+  const cards = screen.queryAllByTestId(/cardItem/i);
+  return cards[Math.floor(Math.random() * cards.length)];
+};
 
+describe("Check the cards of books", () => {
+  beforeEach(() => {
+    render(<App />);
+  });
+
+  it("should render the correct number of cards", () => {
     const cards = screen.queryAllByTestId(/cardItem/i);
 
     expect(cards).toHaveLength(fantasy.length);
   });
+
+  describe("Card Selection", () => {
+    it("should have a red border when selected", () => {
+      const selectedCard = getRandomCard();
+
+      fireEvent.click(selectedCard);
+
+      const computedStyles = window.getComputedStyle(selectedCard);
+
+      expect(computedStyles.borderColor).toBe("red");
+    });
+
+    it("should lose red border when another card is selected", () => {
+      const selectedCard = getRandomCard();
+
+      fireEvent.click(selectedCard);
+
+      const computedStyles = window.getComputedStyle(selectedCard);
+
+      expect(computedStyles.borderColor).toBe("red");
+
+      const selectedCard2 = getRandomCard();
+
+      fireEvent.click(selectedCard2);
+
+      const computedStyles2 = window.getComputedStyle(selectedCard2);
+
+      const computedStyles3 = window.getComputedStyle(selectedCard);
+
+      expect(computedStyles3.borderColor).not.toBe("red");
+
+      expect(computedStyles2.borderColor).toBe("red");
+    });
+  });
 });
 
-describe("check comments area", () => {
-  it("check if the comments area get opened", () => {
+describe("Check comments area", () => {
+  it("should open the comments area", () => {
     render(<App />);
 
     const cardForTest = screen.queryAllByTestId(/cardItem/i);
